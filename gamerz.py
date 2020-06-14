c696be34d0a22a8b3a27ab5c756881b225dccf88
GamerStudio3YT <64490166+GamerStudio3YT@users.noreply.github.com>
2020-06-14 20:00:26 +0200

    Update gamerz.py

--- gamerz.py
+++ gamerz.py
@@ -1,17 +1,15 @@
 import discord
 import asyncio
 import os
-import json
 from discord.ext import commands
 
-#prefixes change database
-def get_prefix(bot, message):
-    with open("prefixes.json", "r") as f:
-        prefixes = json.load(f)
+bot = commands.Bot(command_prefix = "gz.")
 
-    return prefixes[str(message.guild.id)]
-
-bot = commands.Bot(command_prefix = get_prefix)
+#token reader
+def read_token():
+    with open("gamerz-token.txt", "r") as f:
+        lines = f.readlines()
+        return lines[0].strip()
 
 #bot start and status
 @bot.event
@@ -43,41 +41,5 @@ async def clear(ctx, count = 3):
 @bot.command()
 async def cheatcode1(ctx, count = 3):
     await ctx.channel.purge(limit=count)
-
-
-#prefix change
-
-@bot.event
-async def on_guild_join(guild):
-    with open("prefixes.json", "r") as f:
-        prefixes = json.load(f)
-
-    prefixes[str(guild.id)] = "gz."
-
-    with open("prefixes.json", "w") as f:
-        json.dump(prefixes, f, indent=4)
-
-@bot.event
-async def on_guild_remove(guild):
-    with open("prefixes.json", "r") as f:
-        prefixes = json.load(f)
-
-    prefixes.pop(str(guild.id))
-
-    with open("prefixes.json", "w") as f:
-        json.dump(prefixes, f, indent=4)
-
-@bot.command()
-@commands.has_permissions(manage_guild=True)
-async def changeprefix(ctx, prefix):
-    with open("prefixes.json", "r") as f:
-        prefixes = json.load(f)
-
-    prefixes[str(ctx.guild.id)] = prefix
-
-    with open("prefixes.json", "w") as f:
-        json.dump(prefixes, f, indent=4)
-
-    await ctx.send(f"Prefix has been changed to {prefix}")
 
 bot.run(os.environ['Gamerz-Token'])
