0ce37bec514889f70b057d760416484a0874dadb
GamerStudio3YT <channel.gamerstudio3@gmail.com>
2020-07-24 23:18:28 +0200

    added files

--- gamerz.py
+++ gamerz.py
@@ -16,95 +16,36 @@ LEAVE_CHANNEL_ID = 724628622236844173
 async def on_ready():
     print("Logged in as {0.user}".format(bot))
 
-def is_not_pinned(mess):
-    return not mess.pinned
+#Welcome Command
+@bot.event
+async def on_member_join(member):
+    if not member.bot:
+        welcomechannel = discord.utils.get(member.guild.channels, id=WELCOME_CHANNEL_ID)
+        await welcomechannel.send(f"{member.mention} has joined the server. Thank you for joining the server. I hope you have a great time in my server!")
+        print(f"{member} has joined the server.")
+        embed = discord.Embed(
+            title="Welcome to The Sticktuber Community Server",
+            description="""Hi Member. Welcome to The Sticktuber Community. Don't forget to read the rules before chatting. I hope you have a great time in the server. If you have any complains or suggestion then you can dm me at Gamer Studio 3#6531 and if you got banned by mistake and you want to appeal then email me at channel.gamerstudio3@gmail.com """, color=0x22a7f0)
 
-async def status_task():
-    while True:
-        await bot.change_presence(activity=discord.Game("gz.help"), status=discord.Status.online)
-        await asyncio.sleep(10)
-        await bot.change_presence(activity=discord.Game("Official Server Link: https://www.discord.gg/gwkqq7j"), status=discord.Status.online)
-        await asyncio.sleep(8)
+        await member.create_dm()
+        await member.dm_channel.send(embed=embed)
+
+#Leave Command
+@bot.event
+async def on_member_remove(member):
+    leavechannel = discord.utils.get(member.guild.channels, id=LEAVE_CHANNEL_ID)
+    await leavechannel.send(f"{str(member)} has left the server. We hope you will come back again :(")
+    print(f"{str(member)} has left the server")
 
 #help command
 @bot.command()
 async def help(ctx):
-    await ctx.send("""
-    Do you want the help command to be sent in your dm or in this channel? \n
-If you want the help command to be sent in your dm then type **gz.helpdm**
-If you want the help command to be sent in this channel then type **gz.helpchannel**""")
-
-#help command dm
-@bot.command()
-async def helpdm(ctx):
-
-    await ctx.send("Help command has been sent to your dm")
-
-    embed = discord.Embed(
-        title="Gamerz Command Lists",
-        description="""
-        This bot prefix is gz.
-        gz.help
-        gz.clear (You should have manage messages permission to use this command)
-        gz.ping
-        These are the commands. More commands will be added soon. I hope you enjoy this bot
-        
-        Links:
-        Gamerz Add link: https://discordbotlist.com/bots/gamerz
-        Gamerz Bot Offical Server: https://www.discord.gg/gwkqq7j
-        
-        This bot is created by Gamer Studio 3#6531""", color=0x22a7f0)
-
-    await ctx.author.send(embed=embed)
-
-#help command channel
-@bot.command()
-async def helpchannel(ctx):
-
-    embed = discord.Embed(
-        title="Gamerz Command Lists",
-        description="""
-        This bot prefix is gz.
-        gz.help
-        gz.clear (You should have manage messages permission to use this command)
-        gz.ping
-        These are the commands. More commands will be added soon. I hope you enjoy this bot
-
-        Links:
-        Gamerz Add link: https://discordbotlist.com/bots/gamerz
-        Gamerz Bot Offical Server: https://www.discord.gg/gwkqq7j
-
-        This bot is created by Gamer Studio 3#6531""", color=0x22a7f0)
-
-    await ctx.send(embed=embed)
+    await ctx.send("""This Bot is Under Development""")
 
 #ping command
 @bot.command()
 async def ping(ctx):
     await ctx.send(f'Pong! {round(bot.latency * 1000)}ms')
 
-#clear command
-@bot.command()
-@commands.has_permissions(manage_messages=True)
-async def clear(ctx, count = 3):
-    await ctx.channel.purge(limit=count, check=is_not_pinned)
-    await ctx.channel.send(f'{count} message(s) has been cleared')
-
-@clear.error
-async def clear_error(ctx, error):
-    if isinstance(error, CheckFailure):
-         await bot.send_message(ctx.message.channel, "You do not have manage messages permission")
-
-    if isinstance(error, commands.BadArgument):
-        await ctx.send("""Couldn't delete the message. ```Reason: Bad Argument```
-        If you still have this issue then contract me (Gamer Studio 3#6531) or send me an email in gamerzbot1@gmail.com""")
-
-    else:
-        raise error
-
-#cheat code (delete or clear message without needing the permission)
-@bot.command()
-async def cheatcode101(ctx, count = 3):
-    await ctx.channel.purge(limit=count)
-
+#token and run
 bot.run(os.environ['Gamerz-Token'])
